ad261d21df0a1ed1e90dc8e4fd04960fc6bd0c23
Zolice <vividzhenlong@gmail.com>
2022-12-16 13:56:37 +0800

    Feature: Lose Scene
    Made timer more accurate

--- ui.js
+++ ui.js
@@ -1,20 +1,58 @@
-var uiColor = [0, 0, 0]
+var ui_debug = true
+
+var frameRateVar = 60
+
+var uiDefaultFontColor = [0, 0, 0]
 var timer = 0
+
+var uiGameLostBackground = [0, 0, 0, 75]
+var uiGameLostFontColor = [255, 255, 255]
 
 function uiSetup() {
     timer = 0
-    frameRate(60)
+    frameRate(frameRateVar)
 }
 
 function uiDraw() {
-    fill(uiColor)
-    text("FPS: " + Math.floor(frameRate()), 20, 20)
-
-    if (!player.plummeting) {
-        // timer += 1 / getFrameRate() // not used due to infinity
-        timer += 1 / 70 // For some reason the average FPS is 70?
+    if (player.plummeting) {
+        uiGameLost()
     }
 
+    fill(uiDefaultFontColor)
+    textSize(12)
+
     text("Time: " + Math.floor(timer) + "s",
-        20, 40)
+        20, 20)
+
+    text("Score: " + Math.floor(player.score),
+        96, 20)
+
+    if (!player.plummeting) {
+        if (frameRate() > 0) { // Prevents 1 / 0
+            timer += (1 / frameRate())
+        }
+    }
+
+    if(ui_debug){
+        text("FPS: " + Math.floor(frameRate()), 20, 40)
+    }
+}
+
+function uiGameLost() {
+    fill(uiGameLostBackground)
+    rect(0, 0, width, height)
+    fill(uiGameLostFontColor)
+    textSize(128)
+    textAlign(CENTER, CENTER)
+    text("You fell!", width / 2, height / 2)
+    textSize(32)
+    text("Press R to restart", width / 2, height / 2 + 64)
+
+    textSize(24)
+    text("Time: " + Math.floor(timer) + "s",
+        width / 2, height / 2 + 128)
+    text("Score: " + Math.floor(player.score),
+        width / 2, height / 2 + 160)
+
+    textAlign(LEFT, TOP)
 }
